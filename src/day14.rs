99c5c1df75e2a3fee9e704d265363368ed3fbed4
Brian Cooke <cookebri@gmail.com>
2023-12-15 05:51:07 -0800

    day14, part1. happy with this, didn't make me feel dumb. but part 2 I don't have any good ideas on so committing this now before I make a huge mess

--- src/day14.rs
+++ src/day14.rs
@@ -1,26 +1,94 @@
-pub fn day14_part1(_input: &str) -> String {
-    todo!();
+use std::collections::HashMap;
+
+use glam::IVec2;
+
+enum Direction {
+    North,
+    West,
+    South,
+    East,
 }
 
-pub fn day14_part2(_input: &str) -> String {
+enum Rock {
+    Movable,
+    Stable,
+}
+
+fn count_rocks_that_would_fall_to_top(map: &str) -> u32 {
+    // walk each column and count the rocks, O, until we hit a #, then move on.
+    let col_count = map.lines().nth(0).unwrap().chars().count();
+    let height = map.lines().count();
+
+    (0..col_count)
+        .map(|x| {
+            let mut empty_space = 0;
+            map.lines()
+                .enumerate()
+                .map(|(y, line)| match &line[x..=x] {
+                    "O" => height - (y - empty_space),
+                    "." => {
+                        empty_space += 1;
+                        0
+                    }
+                    "#" => {
+                        empty_space = 0;
+                        0
+                    }
+                    _ => unreachable!("inconceivable!"),
+                })
+                .sum::<usize>()
+        })
+        .sum::<usize>() as u32
+}
+
+pub fn day14_part1(input: &str) -> String {
+    count_rocks_that_would_fall_to_top(input).to_string()
+}
+
+fn shift_rocks(map: &mut HashMap<IVec2, Rock>, direction: Direction) {}
+
+pub fn day14_part2(input: &str) -> String {
     todo!();
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use rstest::rstest;
 
-    const INPUT: &str = "";
-
-    #[test]
-    #[ignore]
-    fn test_day14_part1() {
-        assert_eq!("0", day14_part1(INPUT));
+    #[rstest]
+    #[case(
+        "O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....",
+        "136"
+    )]
+    fn test_day14_part1(#[case] input: &str, #[case] expected: &str) {
+        assert_eq!(day14_part1(input), expected);
     }
 
-    #[test]
-    #[ignore]
-    fn test_day14_part2() {
-        assert_eq!("0", day14_part2(INPUT));
+    #[rstest]
+    #[case(
+        "O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....",
+        "64"
+    )]
+    fn test_day14_part2(#[case] input: &str, #[case] expected: &str) {
+        assert_eq!(day14_part2(input), expected);
     }
 }

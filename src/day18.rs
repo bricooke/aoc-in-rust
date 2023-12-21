6ec75d98af32432a338145a1557ef94424bbbe31
Brian Cooke <cookebri@gmail.com>
2023-12-20 20:00:47 -0800

    day18 part 2: another oofer. but. learned. so...yay

--- src/day18.rs
+++ src/day18.rs
@@ -1,6 +1,7 @@
 use std::collections::HashSet;
 
-use glam::IVec2;
+use glam::{I64Vec2, IVec2};
+use itertools::Itertools;
 use nom::{
     bytes::complete::tag,
     character::complete::{self, alpha1, alphanumeric1, newline, space1},
@@ -17,7 +18,7 @@ use nom::{
 //
 // I'm going to parse and store the color information assuming it'll be needed in part 2 but it
 // doesn't affect anything for part 1.
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum Direction {
     North,
     East,
@@ -25,11 +26,11 @@ enum Direction {
     West,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Instruction<'a> {
     direction: Direction,
-    distance: usize,
-    _color: &'a str,
+    distance: i64,
+    color: &'a str,
 }
 
 use Direction::*;
@@ -43,7 +44,7 @@ fn parse_instruction(input: &str) -> IResult<&str, Instruction> {
             "D" => South,
             _ => unreachable!("unexpected direction"),
         }),
-        terminated(complete::u32, space1),
+        terminated(complete::i64, space1),
         delimited(tag("(#"), alphanumeric1, tag(")")),
     ))(input)?;
 
@@ -51,14 +52,13 @@ fn parse_instruction(input: &str) -> IResult<&str, Instruction> {
         input,
         Instruction {
             direction,
-            distance: distance as usize,
-            _color: color,
+            distance,
+            color,
         },
     ))
 }
 
-pub fn day18_part1(input: &str) -> String {
-    let (_, instructions) = separated_list1(newline, parse_instruction)(input).unwrap();
+fn calculate_lagoon_size(instructions: Vec<Instruction>) -> String {
     let mut dugout: HashSet<IVec2> = HashSet::new();
     let mut position = IVec2 { x: 0, y: 0 };
     let mut min_x: i32 = 0;
@@ -69,18 +69,10 @@ pub fn day18_part1(input: &str) -> String {
     for instruction in instructions {
         dugout.insert(position);
         let (x_range, y_range, direction) = match instruction.direction {
-            North => (
-                0..1,
-                -(instruction.distance as i32)..0,
-                IVec2 { x: 0, y: -1 },
-            ),
-            East => (0..(instruction.distance as i32), 0..1, IVec2 { x: 1, y: 0 }),
-            South => (0..1, 0..(instruction.distance as i32), IVec2 { x: 0, y: 1 }),
-            West => (
-                -(instruction.distance as i32)..0,
-                0..1,
-                IVec2 { x: -1, y: 0 },
-            ),
+            North => (0..1, -(instruction.distance)..0, IVec2 { x: 0, y: -1 }),
+            East => (0..(instruction.distance), 0..1, IVec2 { x: 1, y: 0 }),
+            South => (0..1, 0..(instruction.distance), IVec2 { x: 0, y: 1 }),
+            West => (-(instruction.distance)..0, 0..1, IVec2 { x: -1, y: 0 }),
         };
         y_range.for_each(|_| {
             x_range.clone().for_each(|_| {
@@ -95,22 +87,22 @@ pub fn day18_part1(input: &str) -> String {
     }
 
     // draw it (comment this out when running)
-    for y in min_y..max_y {
-        for x in min_y..max_x {
-            if dugout
-                .get(&IVec2 {
-                    x: x as i32,
-                    y: y as i32,
-                })
-                .is_some()
-            {
-                print!("#");
-            } else {
-                print!(".");
-            }
-        }
-        println!("");
-    }
+    // for y in min_y..max_y {
+    //     for x in min_y..max_x {
+    //         if dugout
+    //             .get(&IVec2 {
+    //                 x: x as i32,
+    //                 y: y as i32,
+    //             })
+    //             .is_some()
+    //         {
+    //             print!("#");
+    //         } else {
+    //             print!(".");
+    //         }
+    //     }
+    //     println!("");
+    // }
 
     // now walk each line and fill in positions that are in the polygon
     for y in min_y..max_y {
@@ -139,8 +131,8 @@ pub fn day18_part1(input: &str) -> String {
         }
     }
 
-    println!("-----------------");
-
+    // println!("-----------------");
+    //
     let mut count = 0;
     for y in min_y..max_y {
         for x in min_x..max_x {
@@ -152,19 +144,74 @@ pub fn day18_part1(input: &str) -> String {
                 .is_some()
             {
                 count += 1;
-                print!("#");
-            } else {
-                print!(".");
+                // print!("#");
+                //} else {
+                // print!(".");
             }
         }
-        println!("");
     }
 
     count.to_string()
 }
 
-pub fn day18_part2(_input: &str) -> String {
-    todo!();
+pub fn day18_part1(input: &str) -> String {
+    let (_, instructions) = separated_list1(newline, parse_instruction)(input).unwrap();
+    calculate_lagoon_size(instructions)
+}
+
+pub fn day18_part2(input: &str) -> String {
+    let (_, instructions) = separated_list1(newline, parse_instruction)(input).unwrap();
+
+    // TIL:
+    // shoelace algo + border but I didn't get it.
+    // needed to look up a bunch of hints here :(
+    // most credit: https://github.com/ChristopherBiscardi/advent-of-code/blob/main/2023/rust/day-18/src/part2.rs
+    let positions = instructions
+        .iter()
+        .scan(I64Vec2 { x: 0, y: 0 }, |state, instruction| {
+            let distance = &instruction.color[0..5];
+            let direction = match &instruction.color[5..6] {
+                "0" => I64Vec2 { x: 1, y: 0 },
+                "1" => I64Vec2 { x: 0, y: -1 },
+                "2" => I64Vec2 { x: -1, y: 0 },
+                "3" => I64Vec2 { x: 0, y: 1 },
+                _ => unreachable!(),
+            };
+
+            let distance = i64::from_str_radix(distance, 16).unwrap();
+
+            *state += direction * distance;
+            Some(*state)
+        })
+        .collect_vec();
+
+    let border = positions
+        .iter()
+        .tuple_windows()
+        .map(|(a, b)| {
+            let distance = (*a - *b).abs();
+            distance.x + distance.y
+        })
+        .sum::<i64>()
+        + {
+            let one = positions.iter().last().unwrap();
+            let two = positions.first().unwrap();
+            let distance = (*two - *one).abs();
+            distance.x + distance.y
+        };
+
+    let area = ((positions
+        .iter()
+        .tuple_windows()
+        .map(|(one, two)| one.x * two.y - one.y * two.x)
+        .sum::<i64>()
+        + border)
+        / 2)
+    .abs()
+        + 1;
+
+    dbg!(area);
+    (area + border).to_string()
 }
 
 #[cfg(test)]
@@ -205,9 +252,35 @@ R 2 (#d2c081)
         assert_eq!(day18_part1(input), expected);
     }
 
-    #[test]
-    #[ignore]
-    fn test_day18_part2() {
-        assert_eq!("0", day18_part2(""));
+    #[rstest]
+    #[case(
+        "R 6 (#70c710)
+D 5 (#0dc571)
+L 2 (#5713f0)
+D 2 (#d2c081)
+R 2 (#59c680)
+D 2 (#411b91)
+L 5 (#8ceee2)
+U 2 (#caa173)
+L 1 (#1b58a2)
+U 2 (#caa171)
+R 2 (#7807d2)
+U 3 (#a77fa3)
+L 2 (#015232)
+U 2 (#7a21e3)
+        ",
+        "952408144115"
+    )]
+    #[case(
+        "R 6 (#000060)
+U 5 (#000053)
+L 8 (#000082)
+D 5 (#000051)
+R 2 (#000020)
+",
+        "80"
+    )]
+    fn test_day18_part2(#[case] input: &str, #[case] expected: &str) {
+        assert_eq!(day18_part2(input), expected);
     }
 }
